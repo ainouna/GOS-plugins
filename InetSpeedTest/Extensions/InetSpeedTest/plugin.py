d2644107f30e2c7556c74b35cad2bc2afd12566f
j00zek <j00zek@ubuntu.(none)>
2015-09-24 21:43:56 +0200

    InetSpeedTest initial commit

---
+++ InetSpeedTest/Extensions/InetSpeedTest/plugin.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# InetSpeedTest
+#
+# maintainer: j00zek
+#
+# extension for openpli, all skins, descriptions, bar selections and other @j00zek 2014/2015
+# Uszanuj czyj¹œ pracê i NIE przyw³aszczaj sobie autorstwa!
+
+#This plugin is free software, you are allowed to
+#modify it (if you keep the license),
+#but you are not allowed to distribute/publish
+#it without source code (this version and your modifications).
+#This means you also have to distribute
+#source code of your modifications.
+
+from Plugins.Plugin import PluginDescriptor
+        
+def Plugins(**kwargs):
+    return [PluginDescriptor(name=_("InetSpeedTest"), description=_("Check health of your inet connection"), where = PluginDescriptor.WHERE_PLUGINMENU, fnc=main)]
+
+def main(session, **kwargs):
+    from Screens.Console import Console
+    from Tools.Directories import resolveFilename, SCOPE_PLUGINS
+    runlist = []
+    ScriptPath = resolveFilename(SCOPE_PLUGINS, 'Extensions/InetSpeedTest/')
+    runlist.append( ('chmod 755 %s/speedtest_cli.py' % ScriptPath) )
+    runlist.append( ('%s/speedtest_cli.py' % ScriptPath) )
+    session.open(Console, title = "InetSpeedTest...", cmdlist = runlist)
